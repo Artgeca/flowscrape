e1a35604fa8f109dc73351a516f402956c399e79
artgeca <artgeca@gmail.com>
2024-11-13 17:55:51 +0200

    feat: part 3

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -1,9 +1,23 @@
+'use client';
+
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 import { WorkflowStatus } from '@/types/workflow';
 import { Workflow } from '@prisma/client';
-import { FileTextIcon, PlayIcon } from 'lucide-react';
+import { FileTextIcon, MoreVerticalIcon, PlayIcon, ShuffleIcon, TrashIcon } from 'lucide-react';
 import Link from 'next/link';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import TooltipWrapper from '@/components/TooltipWrapper';
+import { useState } from 'react';
+import DeleteWorkflowDialog from './DeleteWorkflowDialog';
 
 const statusColors = {
   [WorkflowStatus.DRAFT]: 'bg-yellow-400 text-yellow-600',
@@ -16,18 +30,69 @@ const WorkflowCard = ({ workflow }: { workflow: Workflow; }) => {
   return (
     <Card className='overflow-hidden border border-separate rounded-lg shadow-sm hover:shadow-md dark:shadow-primary/30'>
       <CardContent className='p-4 flex items-center justify-between h-[100px]'>
-        <div className={cn('w-10 h-10 rounded-full flex items-center justify-center', statusColors[workflow.status as WorkflowStatus])}>
-          {isDraft ? <FileTextIcon className='w-5 h-5' /> : <PlayIcon className='w-5 h-5 text-white' />}
+        <div className='flex items-center justify-end space-x-3'>
+          <div className={cn('w-10 h-10 rounded-full flex items-center justify-center', statusColors[workflow.status as WorkflowStatus])}>
+            {isDraft ? <FileTextIcon className='w-5 h-5' /> : <PlayIcon className='w-5 h-5 text-white' />}
+          </div>
+          <div>
+            <h3 className='flex items-center text-base font-bold text-muted-foreground'>
+              <Link href={`/workflow/editor/${workflow.id}`} className='flex items-center hover:underline'>
+                {workflow.name}
+              </Link>
+              {isDraft && (
+                <span className='ml-2 px-2 py-0.5 text-sm font-medium bg-yellow-100 text-yellow-800 rounded-full'>
+                  Draft
+                </span>
+              )}
+            </h3>
+          </div>
         </div>
-        <div>
-          <h3 className='flex items-center text-base font-bold text-muted-foreground'>
-            <Link href={`/workflow/editor/${workflow.id}`} className='flex items-center hover:underline'>
-              {workflow.name}
-            </Link>
-          </h3>
+        <div className='flex items-center space-x-2'>
+          <Link href={`/workflow/editor/${workflow.id}`} className={cn(buttonVariants({
+            variant: 'outline',
+            size: 'sm',
+          }), 'flex items-center gap-2')}>
+            <ShuffleIcon size={16} />
+            Edit
+          </Link>
+          <WorkflowActions workflowName={workflow.name} workflowId={workflow.id} />
         </div>
       </CardContent>
     </Card>
+  );
+};
+
+const WorkflowActions = ({ workflowName, workflowId }: { workflowName: string; workflowId: string; }) => {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+  return (
+    <>
+      <DeleteWorkflowDialog
+        open={showDeleteDialog}
+        setOpen={setShowDeleteDialog}
+        workflowName={workflowName}
+        workflowId={workflowId}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={'outline'} size={'sm'}>
+            <TooltipWrapper content={'More actions'}>
+              <div className='flex items-center justify-center w-full h-full'>
+                <MoreVerticalIcon size={18} />
+              </div>
+            </TooltipWrapper>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end'>
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className='flex items-center gap-2 text-destructive' onSelect={() => setShowDeleteDialog((prev) => !prev)}>
+            <TrashIcon size={16} />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 };
 
---
+++ components/TooltipWrapper.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
+
+interface Props {
+  children: React.ReactNode;
+  content: string;
+  side?: 'top' | 'bottom' | 'left' | 'right';
+}
+
+const TooltipWrapper = ({ children, content, side = 'top' }: Props) => {
+  return (
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          {children}
+        </TooltipTrigger>
+        <TooltipContent side={side}>
+          {content}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default TooltipWrapper;
