80299a03f1702bfc848d11ebf3517b504f39c439
artgeca <artgeca@gmail.com>
2024-11-26 17:17:21 +0200

    feat: part 13

--- lib/workflow/task/registry.tsx
+++ lib/workflow/task/registry.tsx
@@ -1,8 +1,14 @@
-import { ExtractTextFromElementTask } from './extractTextFromElement';
-import { LaunchBrowserTask } from './launchBrowser';
-import { PageToHtmlTask } from './pageToHtml';
+import { TaskType } from '@/types/task';
+import { WorkflowTask } from '@/types/workflow';
+import { ExtractTextFromElementTask } from './ExtractTextFromElement';
+import { LaunchBrowserTask } from './LaunchBrowser';
+import { PageToHtmlTask } from './PageToHtml';
 
-export const TaskRegistry = {
+type Registry = {
+  [K in TaskType]: WorkflowTask & { type: K };
+};
+
+export const TaskRegistry: Registry = {
   LAUNCH_BROWSER: LaunchBrowserTask,
   PAGE_TO_HTML: PageToHtmlTask,
   EXTRACT_TEXT_FROM_ELEMENT: ExtractTextFromElementTask,
